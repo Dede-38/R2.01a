7c351778a294825d940f00de2b6fecfdca47014c
Dede-38 <157401827+Dede-38@users.noreply.github.com>
2024-05-03 08:17:59 +0200

    Add files via upload

---
+++ ChateauUtilitaire.java
@@ -0,0 +1,17 @@
+package jeu;
+
+/**
+ *
+ * @author fbm rev hb
+ */
+public class ChateauUtilitaire {
+    public static void afficherChâteau(Chateau chateau) {
+        System.out.println("-----------------------------------------");
+        // Affichage de la couleur du château
+        System.out.println("ORDRE D'ENTRAINEMENT Château " + chateau.getCouleur());
+        // Affichage des guerriers du château
+        for (Guerrier guerrier : chateau.getGuerriersNovices()) {
+            System.out.println(guerrier.getClass()+"_"+ chateau.getCouleur() + "[PV " + guerrier.getPointsDeVie() + "], cout : " + guerrier.getRessourcesPourEntrainement());
+        }
+    }
+}
